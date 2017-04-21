9ce017f8d24c1975b4e2f3ae86148c8b78e2fa90
Zachcodes <Zaspringer@gmail.com>
2017-04-20 20:53:47 -0600

    finished

--- cornerstone/frontend/service.js
+++ cornerstone/frontend/service.js
@@ -1,18 +1,17 @@
 angular.module('myApp')
   .service('myService', function($http, authService) {
     //client service calls
-    var originUrl = process.env.ORIGINHOST
    this.getClient = function(username) {
       return $http({
         method: 'GET',
-        url: `${originUrl}/client/info?username=${username}`
+        url: `/client/info?username=${username}`
         // url: `http://localhost:3200/client/info?username=${username}`
       })
     },
     this.checkLogin = function(username, password) {
       return $http({
         method: 'GET',
-        url: `${originUrl}/client/login?username=${username}&password=${password}`
+        url: `/client/login?username=${username}&password=${password}`
 
         // url: `http://localhost:3200/client/login?username=${username}&password=${password}`
       })
@@ -20,18 +19,18 @@ angular.module('myApp')
     this.getFiles = function(clientid) {
       return $http({
         method: 'GET',
-        url: `${originUrl}/client/files?clientid=${clientid}`
+        url: `/client/files?clientid=${clientid}`
 
         // url: `http://localhost:3200/client/files?clientid=${clientid}`
       })
     },
     this.getInvoices = function(clientid) {
-      return $http.get(`${originUrl}/client/invoices?clientid=${clientid}`)
+      return $http.get(`/client/invoices?clientid=${clientid}`)
 
       // return $http.get(`http://localhost:3200/client/invoices?clientid=${clientid}`)
     }
     this.getClientName = function() {
-      return $http.get(`${originUrl}/client/name`)
+      return $http.get(`/client/name`)
 
       // return $http.get('http://localhost:3200/client/name')
     },
@@ -40,7 +39,7 @@ angular.module('myApp')
     this.newClient = function(name, email) {
       return $http({
         method: 'POST',
-        url: `${originUrl}/new/client`,
+        url: `/new/client`,
         // url: 'http://localhost:3200/new/client',
 
         data: {
@@ -50,14 +49,14 @@ angular.module('myApp')
       })
     },
     this.getNewClient = function(name, email) {
-      return $http.get(`${originUrl}/new/client/created`)
+      return $http.get(`/new/client/created`)
       // return $http.get('http://localhost:3200/new/client/created')
 
     },
     this.newClientLogin = function(username, password, client_id) {
       return $http({
         method: 'POST',
-        url: `${originUrl}/new/client/login`,
+        url: `/new/client/login`,
         // url: 'http://localhost:3200/new/client/login',
 
         data: {
@@ -70,7 +69,7 @@ angular.module('myApp')
     this.addFile = function(filename, filelink, client_id) {
       return $http({
         method: 'POST',
-        url: `${originUrl}/add/file`,
+        url: `/add/file`,
         // url: 'http://localhost:3200/add/file',
         data: {
           filename,
@@ -82,7 +81,7 @@ angular.module('myApp')
     this.addInvoice = function(date, hours, client_id, price, total) {
       return $http({
         method: 'POST',
-        url: `${originUrl}/add/invoice`,
+        url: `/add/invoice`,
         // url: 'http://localhost:3200/add/invoice',
 
         data: {
@@ -100,7 +99,7 @@ angular.module('myApp')
     this.checkAdmin = function(username, password) {
       return $http({
         method: 'GET',
-        url: `${originUrl}/admin/login?username=${username}&password=${password}`
+        url: `/admin/login?username=${username}&password=${password}`
         // url: `http://localhost:3200/admin/login?username=${username}&password=${password}`
 
         })
