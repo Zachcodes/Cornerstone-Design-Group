5db34852171a8729d684fee1323ef5d1669e5849
Zachcodes <Zaspringer@gmail.com>
2017-06-20 12:34:14 -0600

    Changed connectionString for heroku

--- cornerstone/index.js
+++ cornerstone/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const massive = require('massive');
 const port = process.env.PORT
 
-const connectionString = 'postgres://zacharyryanspringer@localhost/cornerstone';
+const connectionString = process.env.DATABASE_URL;
 
 
 
